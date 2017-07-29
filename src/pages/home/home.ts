624208ac158de212b3a6b065bb94ff2eff3256f7
Adrián <mujicagadrian@gmail.com>
2017-07-29 16:55:16 +0200

    Now you can save subject on firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: FirebaseListObservable<any[]>;
 
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+    this.items = db.list('/subject');
   }
 
 }
